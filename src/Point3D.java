2990a7e74d001ac3659abaccee6f7f527fc599c3
HaiKAVT <haivtka@gmail.com>
2021-06-14 16:00:16 +0700

    Bai tap point 2D

---
+++ src/Point3D.java
@@ -0,0 +1,38 @@
+public class Point3D extends Point2D{
+    private float z = 0.0f;
+
+    public Point3D() {
+    }
+
+    public Point3D(float z) {
+        this.z = z;
+    }
+
+    public Point3D(float x, float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public  void  setXYZ(float x, float y , float z){
+        super.setXY(x, y);
+        this.z = z;
+
+    }
+    public float[] getXYZ(){
+        return new float[]{getY() , getY(), getZ()};
+    }
+
+    @Override
+    public String toString() {
+        return "Point3D with z = " + getZ() + " which a subclass of " + super.toString()+
+                '}';
+    }
+}
---
+++ src/Point3DTest.java
@@ -0,0 +1,8 @@
+public class Point3DTest {
+    public static void main(String[] args) {
+        Point3D point3D = new Point3D();
+        System.out.println(point3D);
+        point3D = new Point3D(1.1f, 2.2f,3.3f);
+        System.out.println(point3D);
+    }
+}
